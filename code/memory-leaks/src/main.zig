074bb289465c227c40a34e212ade051ec21a62d6
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-12 02:56:44 +0900

    Update main.zig

--- code/memory-leaks/src/main.zig
+++ code/memory-leaks/src/main.zig
@@ -1,13 +1,18 @@
+//!
+//! メモリリークを検出できる？
+//! https://ziglang.org/learn/samples/
+//!
+
 const std = @import("std");
 
 pub fn main() !void {
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(!general_purpose_allocator.deinit());
 
-    const gpa = general_purpose_allocator.allocator();
+    const allocator = general_purpose_allocator.allocator();
 
-    const u32_ptr = try gpa.create(u32);
-    _ = u32_ptr; // silences unused variable error
+    const u32_ptr = try allocator.create(u32);
+    _ = u32_ptr; // コンパイルエラーを回避
 
-    // oops I forgot to free!
+    // トレースが出力される
 }
