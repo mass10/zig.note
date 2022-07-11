5f09339acb61f30dbb39548a5b3d1ab8c7154465
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-12 03:09:36 +0900

    Update main.zig

--- code/memory-leaks/src/main.zig
+++ code/memory-leaks/src/main.zig
@@ -14,11 +14,18 @@ fn getAllocator() std.mem.Allocator {
 }
 
 pub fn main() !void {
-    // ・std.testing.allocator を使うようにするとよい？
+
+    // 普通のアロケータ
+    // const allocator = std.heap.page_allocator;
+
+    // メモリリーク検出用のアロケータ？
     var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
     defer std.debug.assert(!general_purpose_allocator.deinit());
-
     const allocator = general_purpose_allocator.allocator();
+
+    // テスト用アロケータ？(※ようわからん)
+    // https://ziglang.org/documentation/master/#Report-Memory-Leaks
+    // const allocator = std.testing.allocator;
 
     const u32_ptr = try allocator.create(u32);
     _ = u32_ptr; // コンパイルエラーを回避
