f1be742485fea2e49b0f4dce342c913edc72e185
Irisawa Masaru <irisawa.masaru@gmail.com>
2022-07-17 21:22:07 +0900

    Update main.zig

--- code/highier-order-functions/src/main.zig
+++ code/highier-order-functions/src/main.zig
@@ -6,32 +6,27 @@ const std = @import("std");
 
 /// ハンドラーの型宣言
 ///
-/// @param1 u32 パラメーター
-/// @returns 何も返さない
+/// # Arguments
+/// * `u32` パラメーター
 const HandlerType = fn (_: u32) void;
 
 /// 渡された関数を呼び出すだけの関数
-fn invoke(handler: fn (u32) void, param1: u32) void {
+/// # Arguments
+/// * `handler` HandlerType
+/// * `param1` 整数
+fn invoke(handler: HandlerType, param1: u32) void {
     handler(param1);
-}
-
-/// 文字列を表示するだけの関数
-///
-/// @s 文字列
-fn info(s: []const u8) void {
-    _ = std.log.info("[INFO] {s}", .{s});
 }
 
 /// 整数を表示するだけの関数
 ///
-/// @d 整数
-fn print_digit(d: u32) void {
-    _ = std.log.info("[INFO] Digit: ({d})", .{d});
+/// * `param` 整数
+fn print_digit(param: u32) void {
+    _ = std.log.info("Digit: [{d}]", .{param});
 }
 
 /// エントリーポイントです。
 pub fn main() anyerror!void {
-    info("HELLO");
-
     invoke(print_digit, 1);
+    invoke(print_digit, 100);
 }
